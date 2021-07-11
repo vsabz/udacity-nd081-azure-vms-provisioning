5fc3529d7b74f4e7f0d98a35e72eacf411cdd981
Valeh Sabziyev <valeh.sabziyev@gmail.com>
2021-07-11 14:33:35 +0200

    add more logging for login

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -61,14 +61,17 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        app.logger.info("Current user is authenticated")
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            app.logger.info("Invalid username or password")
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
+        app.logger.info("User logged in")
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
